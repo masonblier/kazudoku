23a1128b9ebbfbcd46a232af94bd070d5cbbd7d6
masonblier <mason.blier@gmail.com>
2017-05-26 23:50:51 -0400

    attribution, readme

--- src/KazudokuApp.jsx
+++ src/KazudokuApp.jsx
@@ -42,6 +42,9 @@ export default class KazudokuApp extends React.Component {
         <div className="game-container">
           <KazudokuGame model={model}/>
         </div>
+        <div className="footer">
+          Implementation of <a href="https://en.wikipedia.org/wiki/Sudoku">Number Place</a>. Easy puzzles sampled from <a href="https://projecteuler.net/problem=96">Project Euler #96</a>. Hard puzzles sampled from <a href="http://staffhome.ecm.uwa.edu.au/~00013890/sudokumin.php">Gordon Royle's Minimum Sudoku file</a>. This implementation is released under the MIT license on <a href="https://github.com/masonblier/kazudoku">Github</a>.
+        </div>
       </div>
     );
   }
